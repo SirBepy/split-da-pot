c996aec0b05d7e25fa042d4e7b6ccca30d51f63c
SirBepy <josipmuzic99@gmail.com>
2026-09-17 13:15:28 +0200

    CHORE: scaffold React + Vite + TS PWA project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <h1>Split Da Pot</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+/// <reference types="vitest/config" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  base: '/split-da-pot/',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Split Da Pot',
+        short_name: 'Split Da Pot',
+        theme_color: '#101014',
+        background_color: '#101014',
+        display: 'standalone',
+        start_url: '.',
+        icons: [
+          {
+            src: '/icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    maxWorkers: 5,
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
